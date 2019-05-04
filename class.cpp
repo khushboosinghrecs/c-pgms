d5d627821996e8ec424cf0dd04c722a881b15239
khushboosinghrecs <41840494+khushboosinghrecs@users.noreply.github.com>
2019-05-04 11:47:13 +0530

    class

--- class.cpp
+++ class.cpp
@@ -45,3 +45,65 @@ int main()
     k.set(6, 8);
     k.view();
 }
+// other class example
+#include<iostream>
+#include<string.h>
+using namespace std;
+class student {
+private:
+    int rollno;
+    char name[20];
+
+    class address {
+    private:
+        int houseno;
+       string street;
+       string state;
+       string pincode;
+
+    public:
+        void setaddress(int h, string s, string c, string n)
+        {
+            houseno = h;
+            street =s;
+            state =c;
+            pincode=n;
+        }
+
+        void showaddress() {
+            cout << houseno << endl;
+            cout << street << " " << state << endl;
+            cout << pincode << endl;
+        }
+        };
+        address add;
+    public:
+        void setrollno(int k) {
+            rollno = k;
+        }
+
+        void setname(char *m)
+        {
+            strcpy(name, m);
+        }
+
+        void setaddress(int h, char *s, char *c, char *n) {
+            add.setaddress(h, s, c, n);
+        }
+
+        void showstudent() {
+
+            cout << rollno;
+            cout << name;
+            add.showaddress();
+        }
+};
+    int main()
+    {
+        student s1;
+        s1.setname("khushi singh");
+        s1.setrollno(100);
+        s1.setaddress(400, "pac", "vns", "12456");
+        s1.showstudent();
+        return 0;
+    }
