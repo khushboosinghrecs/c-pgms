d40daf01086056f801d1ee8e4402a9db70e31eef
khushboosinghrecs <41840494+khushboosinghrecs@users.noreply.github.com>
2019-05-04 12:51:28 +0530

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -176,3 +176,47 @@ int main()
 bos a;
 a.setx(8);
 }
+// new classasssssssssssssssss
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    int testcase;
+    cout<<"enter test case";
+    cin >> testcase;
+    int point;
+    int sizeofstr;
+
+    string strgiven;
+    while(testcase--)
+    {
+    cout<<"enter the sizeofstr point strgiven";
+        cin >> sizeofstr >> point;
+        cin >> strgiven;
+        int max = point;
+        int min = point;
+        for(int i = 0; i < strgiven.size(); i++)
+        {
+            if(strgiven[i]=='L')
+            {
+            cout<<strgiven[i];
+                point = point -1;
+            }else{
+                point = point + 1;
+            }
+
+            if(point > max)
+            {
+                max = point;
+            }
+            if(point < min)
+            {
+                min = point;
+            }
+        }
+
+        cout << (max - (min - 1)) << endl;
+    }
+
+    return 0;
+}
