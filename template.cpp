f03aee9fc4512f869f73cb0705b68b2402594866
khushboosinghrecs <41840494+khushboosinghrecs@users.noreply.github.com>
2019-06-04 23:25:50 +0530

    templates

--- template.cpp
+++ template.cpp
@@ -39,3 +39,38 @@ int main(void)
     cout<<big(8.9,9.0);
     return 0;
 }
+// new pgm
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Array {
+private:
+    T *ptr;
+    int size;
+public:
+    Array(T arr[], int s);
+    void print();
+};
+
+template <typename T>
+Array<T>::Array(T arr[], int s) {
+    ptr = new T[s];
+    size = s;
+    for(int i = 0; i < size; i++)
+        ptr[i] = arr[i];
+}
+
+template <typename T>
+void Array<T>::print() {
+    for (int i = 0; i < size; i++)
+        cout<<" "<<*(ptr + i);
+    cout<<endl;
+}
+
+int main() {
+    char arr[5] = {'a', 'b', 'c', 'd', 'e'};
+    Array<char> a(arr, 5);
+    a.print();
+    return 0;
+}
